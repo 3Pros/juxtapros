269a69089eec6df3defde3ce30bfd6620f8e6cb4
Tyler Mumford <tyler.mumford@icloud.com>
2014-11-21 16:08:53 -0700

    Fixed a bug in the auto-adding.

--- js/more-input.js
+++ js/more-input.js
@@ -2,11 +2,15 @@
 $(".js-lists").on("focus", ".disabled", function () {
 	var disabledRow = $(".input-group.disabled");
 	var newInputGroup = disabledRow.clone()[0];
-	disabledRow.removeClass("disabled");
-	disabledRow.one("keypress", function () {
-		disabledRow.after(newInputGroup);
+	disabledRow.removeClass("disabled").addClass("js-create-on-keypress");
+	$(".js-lists").one("keypress", ".js-create-on-keypress", function () {
+		$(".js-create-on-keypress")
+			.removeClass("js-create-on-keypress")
+			.after(newInputGroup);
 	});
 });
+
+var newDisabledInput = $(".input-group.disabled").clone()[0];
 
 // Count the non-blank inputs in each column
 // $(".js")
