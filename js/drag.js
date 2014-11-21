80cd1cacf9b1c00c007d362e49ae10557b218e59
60015340 <60015340@ironschools.org>
2014-11-21 12:25:04 -0700

    update 1.0.3
    lol removed dragable and changed drag.js

--- js/drag.js
+++ js/drag.js
@@ -1,9 +1,2 @@
-function handleDragStart(e) {
-	this.style.opacity = "0.4";  // this / e.target is the source node.
-	//console.log("We are dragging something.");
-}
-
-$(".column").on("dragstart", handleDragStart);
-		//[].forEach.call(cols, function(col) {
-		//col.addEventListener("dragstart", handleDragStart, false);
-		//});
+$( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
