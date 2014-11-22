0e4dd99998397879ea04b90b4beaa6e20fc014e0
jacobward1898 <jacobward1898@gmail.com>
2014-11-22 02:33:32 -0700

    maybe works?

---
+++ js/nav-active.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    //jquery
+    $(location).attr('href');
+
+    //pure javascript
+    var pathname = window.location.pathname;
+
+    // to show it in an alert window
+    alert(window.location);
+});
