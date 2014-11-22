6f2689dc950e58011f0a2c6ae6ee68d80d305001
jacobward1898 <jacobward1898@gmail.com>
2014-11-22 02:50:47 -0700

    that

--- js/nav-active.js
+++ js/nav-active.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     var theplace = location.pathname;
     var things = theplace.split("/");
-    var filename = things[length-1];
+    var filename = things[things.length-1];
 
 
     alert(filename);
