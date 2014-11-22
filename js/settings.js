eb9c001fe7eb1e449010adb36f30649cc755e9c2
Tyler Mumford <tyler.mumford@icloud.com>
2014-11-22 06:42:46 -0700

    Adding Fold effect.

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,12 @@
 $(".toggle1").on("click", function() {
 	$("#g1").toggleClass("glyphicon-ok");
-	$("input").toggleClass("js-darkblue");
-	$("html").toggleClass("js-blue");
+	$.folded = $(".container").oriDomi({
+		hPanels: 6,
+		speed: 2000,
+	});
+	$.folded.foldUp();
+	// $("input").toggleClass("js-darkblue");
+	// $("html").toggleClass("js-blue");
 });
 $(".toggle2").on("click", function() {
 	$("#g2").toggleClass("glyphicon-ok");
