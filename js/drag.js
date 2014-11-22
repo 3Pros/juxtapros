344be1c2a94466060275a65d12f0302ddeaefb09
Tyler Mumford <tyler.mumford@icloud.com>
2014-11-21 21:26:57 -0700

    Maintain tab ordering after reordering items.

--- js/drag.js
+++ js/drag.js
@@ -3,7 +3,7 @@ $( ".sortable" ).sortable({
 	items: "div:not(.disabled)",
 	handle: "span.input-group-addon",
 	update: function(event, ui) {
-
+		recomputeTabIndices();
 	},
 });
 // $( ".sortable" ).disableSelection();
