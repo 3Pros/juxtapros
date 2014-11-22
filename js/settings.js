9d68142dcefc65bbc3cab88fd679c7d3bf2a60f8
60015340 <60015340@ironschools.org>
2014-11-21 21:35:47 -0700

    added settings fix
    made it so it didnt close when toggled

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,19 @@
-$(".toggle").on("click", function() {
-
-	console.log("it worked!")
+$(".toggle1").on("click", function() {
+	$("#g1").toggleClass("glyphicon-ok");
 
 
 });
+$(".toggle2").on("click", function() {
+	$("#g2").toggleClass("glyphicon-ok");
+
+
+});
+$(".toggle3").on("click", function() {
+	$("#g3").toggleClass("glyphicon-ok");
+
+
+});
+$("#js-set").on("click", function() {
+	$("#js-setme").toggleClass("open");
+
+}); 
