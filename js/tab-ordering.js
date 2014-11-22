e129725fe9b19d15b736cb6b58b8608f195686d4
Tyler Mumford <tyler.mumford@icloud.com>
2014-11-21 21:25:50 -0700

    Correct tab ordering.

---
+++ js/tab-ordering.js
@@ -0,0 +1,18 @@
+function recomputeTabIndices() {
+	console.log("Recomputing tab indices.");
+	$("input")[0].tabIndex = 1;
+	$("input")[1].tabIndex = 2;
+
+	var nextIndex = 3;
+	var lists = $(".js-list")
+	var indexGap = lists.toArray().length;
+
+	lists.each(function (index, element) {
+		$("input", element).each(function (i, elem) {
+			elem.tabIndex = nextIndex + i*indexGap;
+		});
+		++nextIndex;
+	});
+};
+
+recomputeTabIndices();
